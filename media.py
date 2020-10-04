9292ff97260a8c72a8f7655acb8cf4e08ca098fe
beerbin <ryan.karpinski2@gmail.com>
2020-10-04 16:11:02 -0500

    added simple media object

---
+++ media.py
@@ -0,0 +1,26 @@
+class MediaObject:
+
+    def __init__(self, location, name):
+        self.location = location
+        self.name = name
+        self.finalLocation = None
+        self.finalname = None
+
+    def getFinalLocation():
+        return self.finalLocation
+
+    def getName():
+        return self.name
+
+    def getFinalName():
+        return self.finalname
+
+    def getLocation():
+        return self.location
+
+    def setFinalLocation(newLocation):
+        self.finalLocation = newLocation
+
+    def setFinalName(newName):
+        self.finalname = newName
+
