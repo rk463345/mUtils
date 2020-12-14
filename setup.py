93cf46c6697127cbdb8d3e0a1bba88f44358a598
Kyle Brown <kdb424@gmail.com>
2020-12-13 19:46:20 -0800

    setup, gitignore, and pipfile

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+import setuptools
+
+setup(
+    name="mUtils",
+    version="0.1",
+    description="Media file renamer",
+    author="Ryan Karpinski",
+    author_email="ryan.karkinski2@gmail.com",
+    packages=setuptools.find_packages(),
+)
